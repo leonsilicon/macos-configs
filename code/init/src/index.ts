70a5db88dbddd34cf9ac47c102c9349929798dca
Leon Si <contact@leonzalion.com>
2021-12-23 16:19:17 -0500

    update

--- code/init/src/index.ts
+++ code/init/src/index.ts
@@ -4,6 +4,7 @@ import path from 'node:path';
 import { execaCommandSync as exec } from 'execa';
 import { outdent } from 'outdent';
 import open from 'open';
+import inquirer from 'inquirer'
 
 exec(`ssh-keygen -t ed25519 -C 'contact@leonzalion.com'`, {
 	stdio: 'inherit'
@@ -20,8 +21,25 @@ fs.writeFileSync(path.join(os.homedir(), '.ssh/config'), outdent`
 
 exec('ssh-add -K ~/.ssh/id_ed25519', { shell: true })
 
-exec('pbcopy < ~/.ssh/id_ed25519.pub', { shell: true });
-
-console.info('SSH key was copied to clipboard.');
 
 await open('https://github.com/settings/ssh/new');
+for (; ;) {
+	// eslint-disable-next-line no-await-in-loop
+	const input = await inquirer.prompt([
+		{
+			type: 'input',
+			name: 'response',
+			message: 'Enter c to copy SSH key to clipboard, q to quit:'
+		}
+	]) as { response: string };
+	if (input.response === 'c') {
+
+		exec('pbcopy < ~/.ssh/id_ed25519.pub', { shell: true });
+		console.info('SSH key was copied to clipboard.');
+	} else if (input.response === 'q') {
+		break;
+	} else {
+		console.info('Invalid input.')
+	}
+}
+
