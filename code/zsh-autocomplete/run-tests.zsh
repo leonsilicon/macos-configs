8b2694fb703729b0f0bf88d1734ff6fbf3ef9bbb
Leon Si <leon@leonsilicon.com>
2024-04-02 08:28:06 -0400

    chore: remove outdated config

--- code/zsh-autocomplete/run-tests.zsh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh -f
-cd $( git rev-parse --show-toplevel )
-
-env -i HOME=$( mktemp -d ) PATH=$PATH FPATH=$FPATH zsh -f -- \
-    =clitest --list-run --progress dot --prompt '%' --color always \
-        --pre-flight 'git --version; print $PWD $VENDOR $OSTYPE =zsh $ZSH_VERSION $ZSH_PATCHLEVEL' \
-        -- $PWD/.clitest/*.md
